c50da90e5de46c4e24dfb3a21390ce206953f709
SoyPatoYo <matiaspato3@gmail.com>
2023-09-07 20:45:47 -0300

    Update Conexion.java

--- UniversidadGrupo48/src/universidad/accesoADatos/Conexion.java
+++ UniversidadGrupo48/src/universidad/accesoADatos/Conexion.java
@@ -5,31 +5,36 @@ package universidad.accesoADatos;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
 public class Conexion {
 
     private static String url = "jdbc:mariadb://localhost/universidad";
     private static String usuario = "root";
     private static String password = "";
-    private static Connection connection;
+    private static Connection conexion;
 
     private Conexion() throws ClassNotFoundException {
         Class.forName("org.mariadb.jdbc.Driver");
     }
 
-    private Conexion(String url, String usuario, String password) throws ClassNotFoundException {
+    private Conexion(String url, String usuario, String password) {
         this.url = url;
         this.usuario = usuario;
         this.password = password;
-        Class.forName("org.mariadb.jdbc.Driver");
     }
 
-    public static Connection getConexion() throws SQLException, ClassNotFoundException {
-        if (connection == null) {
-            Class.forName("org.mariadb.jdbc.Driver");
-            connection = DriverManager.getConnection(url, usuario, password);
+    public Connection buscarConexion(){
+        if (conexion==null){
+            try{
+                //cargamos las clases de mariadb que implementan JDBC
+                Class.forName("org.mariadb.jdbc.Driver");
+                conexion = DriverManager.getConnection(url, usuario, password);
+            } catch (ClassNotFoundException | SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Error "+ex.getMessage());
+            }
         }
-        return connection;
+        return conexion; //retorna la conexion establecida
     }
 
 }
