e7bf1cdf1361e2bd7c02fb9401ba449c55c7f76f
MarianoRDZ <76136716+mariano3582@users.noreply.github.com>
2023-09-12 18:37:53 -0300

    Update AlumnoData.java

--- UniversidadGrupo48/src/universidad/accesoADatos/AlumnoData.java
+++ UniversidadGrupo48/src/universidad/accesoADatos/AlumnoData.java
@@ -6,8 +6,6 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import org.mariadb.jdbc.Statement;
 import universidad.entidades.Alumno;
