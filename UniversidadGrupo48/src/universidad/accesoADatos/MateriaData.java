47559ab19737aec233b28b6f1fdf5a7ac4d53b9f
SoyPatoYo <matiaspato3@gmail.com>
2023-09-12 20:02:54 -0300

    MateriaData y Pruebas

--- UniversidadGrupo48/src/universidad/accesoADatos/MateriaData.java
+++ UniversidadGrupo48/src/universidad/accesoADatos/MateriaData.java
@@ -5,10 +5,155 @@
  */
 package universidad.accesoADatos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+import org.mariadb.jdbc.Statement;
+import universidad.entidades.Materia;
+
 /**
  *
  * @author Matias
  */
 public class MateriaData {
+
+    private Connection conexion = null;
+
+    public MateriaData() {
+        conexion = Conexion.buscarConexion();
+    }
+
+    public void guardarMateria(Materia materia) {
+
+        String sql = "INSERT INTO materia (nombre, año, estado)"
+                + "VALUES (?, ?, ?)";
+        try {
+            PreparedStatement ps = conexion.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, materia.getNombre());
+            ps.setInt(2, materia.getAnioMateria());
+            ps.setBoolean(3, materia.isEstado());
+            ps.executeUpdate();
+
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                materia.setIdMateria(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "Materia Guardada.");
+            }
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error " + ex.getMessage());
+        }
+
+    }
+
+    public Materia buscarMateria(int id) {
+        String sql = "SELECT nombre, año FROM materia WHERE idMateria=? AND estado=1";
+        Materia materia = null;
+        try {
+            PreparedStatement ps = conexion.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                materia = new Materia();
+                materia.setIdMateria(id);
+                materia.setNombre(rs.getString("nombre"));
+                materia.setAnioMateria(rs.getInt("año"));
+                materia.setEstado(true);
+            } else {
+                JOptionPane.showMessageDialog(null, "No existe una materia con ese id.");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error " + ex.getMessage());
+        }
+        return materia;
+    }
+
+    //Este metodo buscaria materias anque esten dadas de baja. Por si hubiera un usuario que lo necesitara.
+    public Materia buscarCualquierMateria(int id) {
+        String sql = "SELECT nombre, año, estado FROM materia WHERE idMateria=?";
+        Materia materia = null;
+        try {
+            PreparedStatement ps = conexion.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                materia = new Materia();
+                materia.setIdMateria(id);
+                materia.setNombre(rs.getString("nombre"));
+                materia.setAnioMateria(rs.getInt("año"));
+                materia.setEstado(rs.getBoolean("estado"));
+            } else {
+                JOptionPane.showMessageDialog(null, "No existe una materia con ese id.");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error " + ex.getMessage());
+        }
+        return materia;
+    }
+
+    public void modificarMateria(Materia materia) {
+
+        String sql = "UPDATE materia SET nombre=?, año=?, estado=? "
+                + "WHERE idMateria=?";
+
+        try {
+            PreparedStatement ps = conexion.prepareStatement(sql);
+            ps.setString(1, materia.getNombre());
+            ps.setInt(2, materia.getAnioMateria());
+            ps.setBoolean(3, materia.isEstado());
+
+            int exito = ps.executeUpdate();
+            if (exito == 1) {
+                JOptionPane.showMessageDialog(null, "Materia Modificada.");
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error " + ex.getMessage());
+        }
+
+    }
+
+    public void eliminarMateria(int id) {
+        String sql = "UPDATE materia SET estado=0 WHERE idMateria=?";
+
+        try {
+            PreparedStatement ps = conexion.prepareStatement(sql);
+            ps.setInt(1, id);
+            int exito = ps.executeUpdate();
+            if (exito == 1) {
+                JOptionPane.showMessageDialog(null, "Materia Eliminada.");
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error " + ex.getMessage());
+        }
+    }
     
+    public List<Materia> listarMaterias(){
+        String sql="SELECT * FROM materia WHERE estado=1";
+        ArrayList<Materia> materias=new ArrayList<>();
+        
+        try {
+            PreparedStatement ps=conexion.prepareStatement(sql);
+            
+            ResultSet rs=ps.executeQuery();
+            while(rs.next()){
+                Materia materia=new Materia();
+                materia.setNombre(rs.getString("nombre"));
+                materia.setAnioMateria(rs.getInt("año"));
+                materia.setEstado(true);
+                
+                materias.add(materia);
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error " + ex.getMessage());
+        }
+       return materias;
+    }
 }
--- UniversidadGrupo48/src/universidad/universidadMain.java
+++ UniversidadGrupo48/src/universidad/universidadMain.java
@@ -8,7 +8,9 @@ package universidad;
 
 import java.time.LocalDate;
 import universidad.accesoADatos.AlumnoData;
+import universidad.accesoADatos.MateriaData;
 import universidad.entidades.Alumno;
+import universidad.entidades.Materia;
 
 /**
  *
@@ -21,9 +23,20 @@ public class universidadMain {
      */
     public static void main(String[] args) {
         //Alumno x=new Alumno(12345678,"Perez","Maria",LocalDate.of(1990, 5, 23),true);
-        AlumnoData alu=new AlumnoData();
+        //AlumnoData alu=new AlumnoData();
         //alu.guardarAlumno(x);
-        alu.eliminarAlumno(1);
+        //alu.eliminarAlumno(1);
+        //Materia x=new Materia("Matematica",2,true);
+        MateriaData mat=new MateriaData();
+        //mat.eliminarMateria(1);
+        /*for(Materia materia:mat.listarMaterias()){
+            System.out.println("Nombre: "+materia.getNombre());
+            System.out.println("Año: "+materia.getAnioMateria());
+        }*/
+        /*Materia materia=mat.buscarCualquierMateria(1);
+        System.out.println("Nombre: "+materia.getNombre());
+        System.out.println("Año: "+materia.getAnioMateria());
+        System.out.println("Estado: "+materia.isEstado());*/
     }
     
 }
