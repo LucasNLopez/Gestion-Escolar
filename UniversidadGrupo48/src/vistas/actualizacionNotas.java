f979ef9caa8cc584a99e6a5a03dec942e6306770
SoyPatoYo <matiaspato3@gmail.com>
2023-09-25 20:51:54 -0300

    deje todo como Object

--- UniversidadGrupo48/src/vistas/actualizacionNotas.java
+++ UniversidadGrupo48/src/vistas/actualizacionNotas.java
@@ -88,15 +88,7 @@ public class actualizacionNotas extends javax.swing.JPanel {
             new String [] {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
-        ) {
-            Class[] types = new Class [] {
-                java.lang.Integer.class, java.lang.String.class, java.lang.Integer.class, java.lang.Integer.class
-            };
-
-            public Class getColumnClass(int columnIndex) {
-                return types [columnIndex];
-            }
-        });
+        ));
         jScrollPane1.setViewportView(TablaNotas);
 
         jButton6.setText("Modificar");
